bd65541359ed5b73090e9265df0bd610ea398de4
Anna Pawl <annampawl@gmail.com>
2018-10-04 16:50:25 -0700

    users can now see all items

---
+++ app.js
@@ -0,0 +1,36 @@
+// Initializing Handlebars.js in appp
+
+var exphbs = require('express-handlebars');
+
+// Standard Express.js code Modules and Obejcts
+const express = require('express')
+const app = express()
+
+//Database (Eventually)
+
+//Import Routes (eventually)
+//cont items = require ('./controllers/items')
+
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+// INDEX
+app.get('/', (req, res) => {
+  res.render('items-index', { items: items });
+})
+//Middleware (eventually)
+
+//Mock Item Stuff
+let items = [
+    {title: "Great Item", itemTitle: "Strickers"},
+    {title: "Awesome Service", itemTitle: "Badass Haricut"},
+]
+
+
+
+//Server Start
+app.listen(3000, () => {
+  console.log('App listening on port 3000!')
+})
